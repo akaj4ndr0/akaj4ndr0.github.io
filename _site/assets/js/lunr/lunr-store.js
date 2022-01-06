e843a58bbca6c5d5b89dd7f5a848f9d18acbcd44
akaj4ndr0 <akaj4ndr0@gmail.com>
2022-01-06 21:04:59 +0100

    google4

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Programa #Include",
+        "excerpt":"La Fundación GoodJob La Fundación GoodJob desarrolla el Programa #Include con el objetivo de formar en seguridad informática a personas con discapacidad. Se trata de un programa de empleabilidad gratuito, que se desarrolla de forma online, destinado a desarrollar competencias en el sector de la ciberseguridad, una de las profesiones...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/include/",
+        "teaser":null},{
+        "title": "Configuración del laboratorio de pruebas",
+        "excerpt":"Instalación de Virtual Box El primer paso para crear nuestro laboratorio es descargarnos Virtual Box, un software de virtualización para crear máquinas virtuales con instalaciones de sistemas operativos. Esto quiere decir que si tienes un ordenador con Windows, por ejemplo, puedes crear una máquina virtual con cualquier otro sistema operativo...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/laboratorio/",
+        "teaser":null},{
+        "title": "Nmap y pa' dentro",
+        "excerpt":"¿ Qué es Nmap ? Namp (Network Mapper) es una herramienta gratuita y multiplataforma para mapear redes en busca hosts activos, puertos abiertos y servicios corriendo. Nmap viene instalado por defecto en Kali Linux. Port Scanner Como hemos mencionado, con Nmap podemos ver que puertos están abiertos en un determinado...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nmap/",
+        "teaser":null},{
+        "title": "Que no te lo cuenten. Ataque Zero-Day",
+        "excerpt":"¿ Qué es un ataque Zero-Day ? Un Ataque Zero-Day es un ciberataque contra una aplicación o sistema que tiene como objetivo la ejecución de código malicioso gracias al conocimiento de una vulnerabilidad desconocida por el fabricante del producto; lo que supone que el fabricante del producto aún no ha...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/quenotelocuentem/",
+        "teaser":null}]
